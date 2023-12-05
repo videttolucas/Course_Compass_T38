09d5160e40eb9832c3689ccbafc22d297f36aaf6
videttolucas <lvidetto@nevada.unr.edu>
2023-12-05 08:16:55 -0800

    Testing

--- backend_venv/backend.py
+++ backend_venv/backend.py
@@ -4,9 +4,36 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from mysql.connector import connect
+from datetime import datetime, timedelta
+import hashlib
+import os
 
 app = Flask(__name__)
 CORS(app)
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    email = data['email']
+    password = data['password']
+    
+    connection = getConnection()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM tblUser WHERE Email = %s", (email, ))
+    user = cursor.fetchone()
+    
+    if user and hashlib.sha256(password.encode()).hexdigest() == user['Passwd']:
+        token = hashlib.sha256(os.urandom(64)).hexdigest()
+        expiration = datetime.now() + timedelta(hours = 48)
+        
+        # Store token and token expiration in database
+        
+        return  jsonify({"message": "Login successful", "user": user, "token": token, "expires": expiration.isoformat()}), 200
+    else:
+        return jsonify({"message": "Incorrect username or password"}), 401
+    
+    cursor.close()
+    connection.close()
 
 @app.route('/api/courses', methods=['GET'])
 def getCourses():
@@ -18,14 +45,6 @@ def getCourses():
     connection.close()
     return jsonify(courses)
 
-@app.route('/api/courses', methods=['POST'])
-def addCourse():
-    newCourse = request.json
-    ########
-    ########
-    ########
-    return jsonify(newCourse), 201
-
 def getConnection():
     connection = connect(
         host='####', # Replace with database credentials
@@ -34,3 +53,6 @@ def getConnection():
         database='####'
     )
     return connection
+
+#if __name__ == '__main--':
+    #app.run(debug=True)
