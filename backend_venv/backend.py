512f5d8e128cb898c49184abc7028f99035318b5
videttolucas <lvidetto@nevada.unr.edu>
2023-12-06 05:47:37 -0800

    Implemented login/demo login with redirect to home screen and recognition from backend

--- backend_venv/backend.py
+++ backend_venv/backend.py
@@ -1,7 +1,7 @@
 # Created by Lucas Videtto
 # Backend connection for Course Compass
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, session
 from flask_cors import CORS
 from mysql.connector import connect, Error
 from datetime import datetime, timedelta
@@ -9,8 +9,43 @@ import hashlib
 import os
 
 app = Flask(__name__)
+app.secret_key = '12345'
 CORS(app)
 
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    email = data['email']
+    password = data['password']
+    
+    demo_email = '425demo@example.com' # For demo showcase / plan b
+    demo_pw = 'Password1!'
+    
+    if email == demo_email and password == demo_pw:
+        print("User logged in and session started") # Print check for testing purposes
+        session['user_email'] = email
+        return jsonify({"message": "Demo login successful"}), 200
+    
+    connection = connectToDB()
+    if connection is not None:
+        cursor = connection.cursor(dictionary=True)
+        try:
+            cursor.execute("SELECT * FROM cs425.tblUser WHERE Email = %s", (email,))
+            user = cursor.fetchone()
+            if user and user['Passwd'] == password:
+                session['user_email'] = email
+                return jsonify({"message": "Login successful"}), 200
+            else:
+                return jsonify({"error": "Invalid email or password"}), 401
+        except Error as err:
+            print("Error while authenticating user", err)
+            return jsonify({"error": str(err)}), 500
+        finally:
+            cursor.close()
+            connection.close()
+    else:
+        return jsonify({"error": "Database connection failed"}), 500
+    
 @app.route('/signup', methods=['POST'])
 def signup():
     data = request.json
