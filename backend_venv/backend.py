d281342a50ce63b6617c8ec797643b9ddb57fde8
videttolucas <lvidetto@nevada.unr.edu>
2023-12-05 22:49:45 -0800

    Cleaned up backend files for demo and fixed errors with .vue files

--- backend_venv/backend.py
+++ backend_venv/backend.py
@@ -11,64 +11,70 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/login', methods=['POST'])
-def login():
-    data = request.json
-    email = data['Email']
-    password = data['Passwd']
+# @app.route('/login', methods=['POST'])
+# def login():
+#     data = request.json
+#     email = data['Email']
+#     password = data['Passwd']
     
-    connection = getConnection()
-    cursor = connection.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM tblUser WHERE Email = %s", (email, ))
-    user = cursor.fetchone()
+#     connection = getConnection()
+#     cursor = connection.cursor(dictionary=True)
+#     cursor.execute("SELECT * FROM tblUser WHERE Email = %s", (email, ))
+#     user = cursor.fetchone()
     
-    if user and hashlib.sha256(password.encode()).hexdigest() == user['Passwd']:
-        token = hashlib.sha256(os.urandom(64)).hexdigest()
-        expiration = datetime.now() + timedelta(hours = 48)
+#     if user and hashlib.sha256(password.encode()).hexdigest() == user['Passwd']:
+#         token = hashlib.sha256(os.urandom(64)).hexdigest()
+#         expiration = datetime.now() + timedelta(hours = 48)
         
-        # Store token and token expiration in database (not necessary for demo)
-        cursor.close()
-        connection.close()
-        return  jsonify({"message": "Login successful", "user": user, "token": token, "expires": expiration.isoformat()}), 200
-    else:
-        cursor.close()
-        connection.close()
-        return jsonify({"message": "Incorrect username or password"}), 401
+#         # Store token and token expiration in database (not necessary for demo)
+#         cursor.close()
+#         connection.close()
+#         return  jsonify({"message": "Login successful", "user": user, "token": token, "expires": expiration.isoformat()}), 200
+#     else:
+#         cursor.close()
+#         connection.close()
+#         return jsonify({"message": "Incorrect username or password"}), 401
     
-@app.route('/signup', method=['POST'])
-def signup():
-    data = request.json
-    firstName = data['Fname']
-    lastName = data['Lname']
-    email = data['Email']
-    pw = hashlib.sha256(data['Passwd'].encode()).hexdigest()
-    majorId = data['majorID']
+# @app.route('/signup', method=['POST'])
+# def signup():
+#     data = request.json
+#     firstName = data['Fname']
+#     lastName = data['Lname']
+#     email = data['Email']
+#     pw = hashlib.sha256(data['Passwd'].encode()).hexdigest()
+#     majorId = data['majorID']
     
-    connection = getConnection()
-    cursor = connection.cursor()
+#     connection = getConnection()
+#     cursor = connection.cursor()
     
-    cursor.execute("SELECT * FROM tblUser WHERE Email = %s", (email,))
-    if cursor.fetchone():
-        return jsonify({"message": "Email already registered"}), 409
+#     cursor.execute("SELECT * FROM tblUser WHERE Email = %s", (email,))
+#     if cursor.fetchone():
+#         return jsonify({"message": "Email already registered"}), 409
     
-    cursor.execute(
-        "INSERT INTO tblUser (Fname, Lname, Email, Passwd, majorID) VALUES (%s, %s, %s, %s, %s)",
-        (firstName, lastName, email, pw, majorId))
+#     cursor.execute(
+#         "INSERT INTO tblUser (Fname, Lname, Email, Passwd, majorID) VALUES (%s, %s, %s, %s, %s)",
+#         (firstName, lastName, email, pw, majorId))
     
-    connection.commit()
-    cursor.close()
-    connection.close()
+#     connection.commit()
+#     cursor.close()
+#     connection.close()
     
-    return jsonify({"message": "Successfully created new account"}), 201
+#     return jsonify({"message": "Successfully created new account"}), 201
 
 def getConnection():
     connection = connect(
-        host='####', # Replace with database credentials
-        user='####',
-        password='####',
-        database='####'
+        host='aws.connect.psdb.cloud',
+        user='kvq86gq6g04wsq6lz3vi',
+        password='pscale_pw_mJzru53sidNF7YadEPHFKE0ATfvcJK9EFgBQxy7JCCa',
+        database='cs425'
     )
+    
+    if connection:
+        print('Connection successful')
+        
     return connection
+
+getConnection()
 
 if __name__ == '__main--':
     app.run(debug=True)
--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -3,42 +3,12 @@
 </template>
 
 <script>
-import Login from '@/components/Login.vue'
+  import Login from '@/components/Login.vue'
 
-export default {
-  name: 'LoginView',
-  components: {
-    Login
-  },
-  data() {
-    return {
-      email: '',
-      password: '',
-      errorMessage: ''
-    };
-  },
-  methods: {
-    async login() {
-      try {
-        const response = await this.axios.post('http://localhost:5000/login', {
-          email: this.email,
-          password: this.password
-        });
-        
-        const { token, expires } = response.data;
-        
-        localStorage.setItem('userToken', token);
-        localStorage.setItem('tokenExpiration', expires);
-
-        this.$router.push('/home');  // Redirect to home
-      } catch (error) {
-        if (error.response && error.response.status === 401) {
-          this.errorMessage = 'Incorrect email or password. Please try again.';
-        } else {
-          this.errorMessage = 'An error occurred. Please try again later.';
-        }
-      }
+  export default{
+    name: 'LoginView',
+    components: {
+      Login
     }
   }
-}
 </script>
