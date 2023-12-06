01fc0653543e78ed876e81b9886c741a341e5b13
videttolucas <lvidetto@nevada.unr.edu>
2023-12-06 03:19:07 -0800

    Revised connectToDB method, enhanced DB credential security, refined signup method in backend, implemented successful interactivity between frontend signup page and backend server

--- backend_venv/backend.py
+++ backend_venv/backend.py
@@ -3,7 +3,7 @@
 
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from mysql.connector import connect
+from mysql.connector import connect, Error
 from datetime import datetime, timedelta
 import hashlib
 import os
@@ -11,70 +11,43 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-# @app.route('/login', methods=['POST'])
-# def login():
-#     data = request.json
-#     email = data['Email']
-#     password = data['Passwd']
+@app.route('/signup', methods=['POST'])
+def signup():
+    data = request.json
+    firstname = data['firstname']
+    lastname = data['lastname']
+    email = data['email']
+    password = data['password']
+    majorID = data['majorID']
     
-#     connection = getConnection()
-#     cursor = connection.cursor(dictionary=True)
-#     cursor.execute("SELECT * FROM tblUser WHERE Email = %s", (email, ))
-#     user = cursor.fetchone()
-    
-#     if user and hashlib.sha256(password.encode()).hexdigest() == user['Passwd']:
-#         token = hashlib.sha256(os.urandom(64)).hexdigest()
-#         expiration = datetime.now() + timedelta(hours = 48)
-        
-#         # Store token and token expiration in database (not necessary for demo)
-#         cursor.close()
-#         connection.close()
-#         return  jsonify({"message": "Login successful", "user": user, "token": token, "expires": expiration.isoformat()}), 200
-#     else:
-#         cursor.close()
-#         connection.close()
-#         return jsonify({"message": "Incorrect username or password"}), 401
-    
-# @app.route('/signup', method=['POST'])
-# def signup():
-#     data = request.json
-#     firstName = data['Fname']
-#     lastName = data['Lname']
-#     email = data['Email']
-#     pw = hashlib.sha256(data['Passwd'].encode()).hexdigest()
-#     majorId = data['majorID']
-    
-#     connection = getConnection()
-#     cursor = connection.cursor()
-    
-#     cursor.execute("SELECT * FROM tblUser WHERE Email = %s", (email,))
-#     if cursor.fetchone():
-#         return jsonify({"message": "Email already registered"}), 409
-    
-#     cursor.execute(
-#         "INSERT INTO tblUser (Fname, Lname, Email, Passwd, majorID) VALUES (%s, %s, %s, %s, %s)",
-#         (firstName, lastName, email, pw, majorId))
-    
-#     connection.commit()
-#     cursor.close()
-#     connection.close()
-    
-#     return jsonify({"message": "Successfully created new account"}), 201
+    connection = connectToDB()
+    if connection is not None:
+        cursor = connection.cursor()
+        try:
+            insertQuery = """INSERT INTO cs425.tblUser (Fname, Lname, Email, Passwd, majorID) VALUES (%s, %s, %s, %s, %s)"""
+            cursor.execute(insertQuery, (firstname, lastname, email, password, majorID))
+            connection.commit()
+            return jsonify({"message": "Signup successful"}), 200
+        except Error as err:
+            print("Error while inserting data into database", err)
+            return jsonify({"error": str(err)}), 500
+        finally:
+            cursor.close()
+            connection.close()
+    else:
+        return jsonify({"error": "Database connection failed"}), 500
 
-def getConnection():
-    connection = connect(
-        host='aws.connect.psdb.cloud',
-        user='kvq86gq6g04wsq6lz3vi',
-        password='pscale_pw_mJzru53sidNF7YadEPHFKE0ATfvcJK9EFgBQxy7JCCa',
-        database='cs425'
-    )
-    
-    if connection:
-        print('Connection successful')
-        
-    return connection
+def connectToDB():
+    try:
+        connection = connect(
+            host=os.getenv('DB_HOST', 'default_host'),
+            database=os.getenv('DB_NAME', 'default_db_name'),
+            user=os.getenv('DB_USER', 'default_user'),
+            password=os.getenv('DB_PASSWORD', 'default_password')
+        )
+        return connection
+    except Error as err:
+        print("Error while connecting to database", err)
+        return None
 
-getConnection()
-
-if __name__ == '__main--':
-    app.run(debug=True)
+app.run(debug=True)
